ed79f4d6541ae7fc05070a2694dab2d17f9bcb4c
Jabou19 <jabou19@student.sdu.dk>
2025-03-25 18:04:36 +0100

    okay

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,14 @@ import scheduler.Simulator;
 import utils.TaskLoader;
 
 import java.io.IOException;
+import java.net.SocketTimeoutException;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        List<Task> tasks = TaskLoader.loadTasks("files/exercise-TC1 - Copy.csv");
+        String fileName = "files/exercise-TC1 - Copy.csv";
+        List<Task> tasks = TaskLoader.loadTasks(fileName);
+        System.out.println("file name is "+fileName);
         int simulationTime = 100; // Set the simulation for 100 time units.
         Simulator simulator = new Simulator(tasks, simulationTime);
         simulator.run();
