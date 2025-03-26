2417c690fc8675d63c2e5e0e4c3034a657148ce2
Jabou19 <jabou19@student.sdu.dk>
2025-03-26 21:03:30 +0100

    clean code that does not used

--- src/scheduler/RTA.java
+++ src/scheduler/RTA.java
@@ -15,46 +15,6 @@ public class RTA {
     public boolean RTAAnalysis() {
         // sort the tasks by priority(the shorter the period, the higher the priority)
         tasks.sort((t1, t2) -> Integer.compare(t1.period, t2.period));
-//        // Analyze each task
-//        for (int i = 0; i < tasks.size(); i++) {
-//            Task task = tasks.get(i);
-//            int I = 0;
-//            int iteration = 0;
-//
-//            while (true) {
-//                int R = I + task.wcet;
-//
-//                if (R > task.deadline) {
-//                    System.out.println("UNSCHEDULABLE");
-//                    return false;
-//                }
-//
-//                int newI = 0;
-//                for (int j = 0; j < i; j++) { // only higher-priority tasks
-//                    Task hpTask = tasks.get(j);
-//                    newI += Math.ceil((double) R / hpTask.period) * hpTask.wcet;
-//                }
-//
-//                if (newI == I) {
-//                    task.wcrt = R;
-//                    break;
-//                }
-//
-//                I = newI;
-//
-//                iteration++;
-//                if (iteration > 1000) {
-//                    System.out.println("Exceeded max iterations for task: " + task.name);
-//                    return false;
-//                }
-//            }
-//
-////            System.out.println("Task " + task.name + " WCRT: " + task.wcrt);
-//        }
-//
-//        System.out.println("SCHEDULABLE");
-//        return true;
-//    }
 
         // analysis every tasks
         for (Task task : tasks) {
--- src/scheduler/Simulator.java
+++ src/scheduler/Simulator.java
@@ -4,62 +4,6 @@ import model.Job;
 import model.Task;
 
 import java.util.*;
-
-//public class Simulator {
-//    private List<Task> tasks;
-//    private PriorityQueue<Job> readyQueue;
-//    private int simulationTime;
-//    private Map<Task, Integer> worstCaseResponseTime;
-//
-//    public Simulator(List<Task> tasks, int simulationTime) {
-//        this.tasks = tasks;
-//        this.simulationTime = simulationTime;
-//        this.readyQueue = new PriorityQueue<>(Comparator.comparingInt(j -> j.task.period));
-//        this.worstCaseResponseTime = new HashMap<>();
-//    }
-//
-//    public void run() {
-//        int currentTime = 0;
-//        tasks.sort((t1, t2) -> Integer.compare(t1.period, t2.period));
-//
-//        while (currentTime <= simulationTime) {
-//            // Release a new task
-//            for (Task task : tasks) {
-//                if (currentTime % task.period == 0) {
-//                    Job newJob = new Job(task, currentTime);
-//                    readyQueue.add(newJob);
-//                }
-//            }
-//
-//            // Choose the highest-priority task from all tasks
-//            if (!readyQueue.isEmpty()) {
-//                Job currentJob = readyQueue.poll();
-//                int execTime = Math.min(currentJob.executionTime, 1); // Executing one time unit.
-//                currentTime += execTime;
-//                currentJob.executionTime -= execTime;
-//
-//                // Computing WCRT
-//                if (currentJob.executionTime <= 0) {
-//                    int responseTime = currentTime - currentJob.releaseTime;
-//                    worstCaseResponseTime.put(currentJob.task,
-//                            Math.max(worstCaseResponseTime.getOrDefault(currentJob.task, 0), responseTime));
-//                } else {
-//                    readyQueue.add(currentJob); // If tasks are unfinished, then continue executing
-//                }
-//            } else {
-//                currentTime++; // Spare time
-//            }
-//        }
-//
-//        // output results
-//        for (Task task : tasks) {
-//            System.out.println("Task " + task.name + " WCRT: " + worstCaseResponseTime.getOrDefault(task, 0));
-//        }
-//    }
-//}
-
-import java.util.*;
-
 public class Simulator {
     private List<Task> tasks; // List of tasks
     private List<Job> readyQueue; // Ready queue
