48de048571cd50067b4111de43999b203ed66125
Jabou19 <jabou19@student.sdu.dk>
2025-03-26 21:59:16 +0100

    changed

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        List<Task> tasks = TaskLoader.loadTasks("files/exercise-TC3 - Copy.csv");
+        List<Task> tasks = TaskLoader.loadTasks("files/exercise-TC3.csv");
         int simulationTime = 10000; // Set the simulation for 100 time units.
         Simulator simulator = new Simulator(tasks, simulationTime);
         simulator.run();
